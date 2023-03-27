9bc35b1e115d61115c0c5244d76e95dfbba27d62
LastDawg <127403892+LastDawg@users.noreply.github.com>
2023-03-27 13:19:51 +0300

    Открывание автодверей
    Требуется изменение модели автомобиля, на старых работать не будет

--- src/xrGame/Car.cpp
+++ src/xrGame/Car.cpp
@@ -536,7 +536,7 @@ void CCar::Hit(SHit* pHDS)
     //	CExplosive::SetInitiator(HDS.who->ID());
     //}
     WheelHit(HDS.damage(), HDS.bone(), HDS.hit_type);
-    DoorHit(HDS.damage(), HDS.bone(), HDS.hit_type);
+    //DoorHit(HDS.damage(), HDS.bone(), HDS.hit_type);
     float hitScale = 1.f, woundScale = 1.f;
     if (HDS.hit_type != ALife::eHitTypeStrike)
         CDamageManager::HitScale(HDS.bone(), hitScale, woundScale);
