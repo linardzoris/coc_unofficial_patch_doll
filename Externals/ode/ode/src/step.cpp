f2cdd26c77161a46bdc03e9df9a18bc9b3b5138b
LastDawg <127403892+LastDawg@users.noreply.github.com>
2023-07-29 16:55:14 +0300

    Фикс отсюда https://github.com/DanceManiac/Advanced-X-Ray-Public/commit/26e4d38f092d89318e0c26057a66b1fa4a956c4a

--- Externals/ode/ode/src/step.cpp
+++ Externals/ode/ode/src/step.cpp
@@ -497,7 +497,11 @@ void dInternalStepIsland_x1 (dxWorld *world, dxBody * const *body, int nb,
 # ifdef TIMING
   dTimerNow ("update position");
 # endif
-  for (i=0; i<nb; i++) dxStepBody (body[i],stepsize);
+  for (i = 0; i < nb; i++)
+  {
+      if ((body[i]->flags & dxBodyNoUpdatePos) == 0)
+          dxStepBody(body[i], stepsize);
+  }
 
 # ifdef TIMING
   dTimerNow ("tidy up");
@@ -941,7 +945,11 @@ void dInternalStepIsland_x2 (dxWorld *world, dxBody * const *body, int nb,
 # ifdef TIMING
   dTimerNow ("update position");
 # endif
-  for (i=0; i<nb; i++) dxStepBody (body[i],stepsize);
+  for (i = 0; i < nb; i++)
+  {
+      if ((body[i]->flags & dxBodyNoUpdatePos) == 0)
+          dxStepBody(body[i], stepsize);
+  }
 
 # ifdef COMPARE_METHODS
   dReal *tmp_vnew = (dReal*) ALLOCA (nb*6*sizeof(dReal));
