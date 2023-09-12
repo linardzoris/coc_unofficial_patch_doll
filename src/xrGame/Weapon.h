23e998502843c69b5b6d23d48082e867f886aec4
LastDawg <127403892+LastDawg@users.noreply.github.com>
2023-09-12 16:03:31 +0300

    Revert "Профет подсказал"
    This reverts commit 3b93c9c673f7118e6d1bcecb17f377b29cd132ba.

--- src/xrGame/Weapon.h
+++ src/xrGame/Weapon.h
@@ -239,7 +239,6 @@ public:
     bool IsSilencerAttached() const;
     bool IsLaserAttached() const;
     bool bGrenadeLauncherNSilencer;
-    bool HaveCartridgeInInventory(u8 cnt);
 
     virtual bool GrenadeLauncherAttachable();
     virtual bool ScopeAttachable();
--- src/xrGame/WeaponShotgun.h
+++ src/xrGame/WeaponShotgun.h
@@ -35,6 +35,7 @@ protected:
     void TriStateReload();
     virtual void OnStateSwitch(u32 S, u32 oldState);
 
+    bool HaveCartridgeInInventory(u8 cnt);
     virtual u8 AddCartridge(u8 cnt);
 
     ESoundTypes m_eSoundOpen;
